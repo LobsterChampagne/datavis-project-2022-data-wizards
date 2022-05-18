3ccc89076c2f9d6aec2a53491225ce79ab164af4
LobsterChampagne <ferdinand.ytteborg@epfl.ch>
2022-05-19 01:11:07 +0200

    add axis to line chart

--- lineChart.js
+++ lineChart.js
@@ -7,6 +7,43 @@ function whenDocumentLoaded(action) {
 	}
 }
 
+const width = 700, height = 450;
+
+const svg = d3.select("#lineChart")
+	.attr("width", width)
+	.attr("height", height);
+
+d3.csv('Data/release_year.csv')
+	.then( data => {
+		console.log()
+
+		var x = d3.scaleLinear()
+			.domain(d3.extent(data, d => d.release_year))
+			.range([ 0, width - 45 ]);
+		svg.append("g")
+			.attr("transform", `translate(30, ${height - 30})`)
+			.call(d3.axisBottom(x).ticks(5));
+
+		var y = d3.scaleLinear()
+			.domain([0, d3.max(data, d => d.count)])
+			.range([ height - 45, 0 ]);
+		svg.append("g")
+			.attr("transform", `translate(30, 15)`)
+			.call(d3.axisLeft(y))
+
+		const line = d3.line()
+			.x(d => x(d.release_year))
+			.y(d => y(d.count));
+		 svg.selectAll("path.line")
+			.data(data)
+			.join("path")
+			  .attr("class", "line")
+			  .attr("fill", "none")
+			  .style("stroke", 'red')
+			  .attr("d", d => line(d));
+	}
+)
+
 whenDocumentLoaded(() => {
     console.log("line Chart: Do what ever you want here")
 	//plot_object = new MapPlot('map-plot');
