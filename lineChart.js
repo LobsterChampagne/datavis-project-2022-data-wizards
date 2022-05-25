d2f293f3cd7106a2b2ae2650aa91796b2fea483c
LobsterChampagne <ferdinand.ytteborg@epfl.ch>
2022-05-25 11:58:34 +0200

    change variable names linechart to unique

--- lineChart.js
+++ lineChart.js
@@ -7,11 +7,11 @@ function whenDocumentLoaded(action) {
 	}
 }
 
-const width = 700, height = 450;
+const lineChartWidth = 700, lineChartHeight = 450;
 
-const svg = d3.select("#lineChart")
-	.attr("width", width)
-	.attr("height", height);
+const lineSvg = d3.select("#lineChart")
+	.attr("width", lineChartWidth)
+	.attr("height", lineChartHeight);
 
 d3.csv('Data/release_year.csv')
 	.then( data => {
@@ -19,28 +19,28 @@ d3.csv('Data/release_year.csv')
 
 		var x = d3.scaleLinear()
 			.domain(d3.extent(data, d => d.release_year))
-			.range([ 0, width - 45 ]);
-		svg.append("g")
-			.attr("transform", `translate(35, ${height - 30})`)
+			.range([ 0, lineChartWidth - 45 ]);
+		lineSvg.append("g")
+			.attr("transform", `translate(35, ${lineChartHeight - 30})`)
 			.call(d3.axisBottom(x).ticks(5));
 
 		var y = d3.scaleLinear()
 			.domain([0, d3.max(data, d => +d.count)])
-			.range([ height - 45, 0 ]);
-		svg.append("g")
+			.range([ lineChartHeight - 45, 0 ]);
+		lineSvg.append("g")
 			.attr("transform", `translate(35, 15)`)
 			.call(d3.axisLeft(y))
 
 		const line = d3.line()
 			.x(d => x(d.release_year))
 			.y(d => y(d.count));
-		svg.append("path")
+		lineSvg.append("path")
 			.datum(data)
 			.attr("fill", "none")
 			.attr("stroke", "steelblue")
 			.attr("stroke-linejoin", "round")
 			.attr("stroke-linecap", "round")
-			.attr("stroke-width", 1.5)
+			.attr("stroke-lineChartWidth", 1.5)
 			.attr("transform", `translate(35, 15)`)
 			.attr("d", line);
 	}
